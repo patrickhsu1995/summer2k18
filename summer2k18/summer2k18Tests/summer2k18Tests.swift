0a6915361dea8a18dc0fb4f32487749d0639c66a
Steven <gglpsteven@gmail.com>
2018-05-29 15:58:46 -0400

    Starter code

---
+++ summer2k18/summer2k18Tests/summer2k18Tests.swift
@@ -0,0 +1,36 @@
+//
+//  summer2k18Tests.swift
+//  summer2k18Tests
+//
+//  Created by Steven Chen on 2018-05-29.
+//  Copyright © 2018 gglpsteven. All rights reserved.
+//
+
+import XCTest
+@testable import summer2k18
+
+class summer2k18Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
